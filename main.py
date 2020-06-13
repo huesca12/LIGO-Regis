8b31b200a6e18411f732fac7dc259cae39b42913
huesca12 <thebritishspy12@gmail.com>
2020-06-13 19:22:50 +0100

    data preparation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#import the important stuff
+#IMPORTS
+#import external packages
 import numpy as np
 from sklearn.manifold import TSNE
 %matplotlib inline
@@ -9,3 +10,18 @@ from sklearn.datasets import fetch_mldata
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import pandas as pd
+
+#import local modules
+
+#
+#define the data filename
+filename = 'data/gspy_o3a.csv' #your file here with a .csv ending
+
+#read the csv file into a dataframe
+rawDf = pd.read_csv(filename)
+
+#define the list of columns that we want to drop
+dropList = ['chisq','chisqDof','confidence','GPStime','ifo','imgUrl','id']
+
+#drop the columns we don't need
+mainDf = rawDf.drop(columns=dropList)
