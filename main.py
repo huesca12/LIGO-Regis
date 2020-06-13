3fc7ed637287074eb412e2b08e3cefdd7856aac2
huesca12 <thebritishspy12@gmail.com>
2020-06-13 19:20:48 +0100

    init with imports

---
+++ main.py
@@ -0,0 +1,11 @@
+#import the important stuff
+import numpy as np
+from sklearn.manifold import TSNE
+%matplotlib inline
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+from sklearn.datasets import fetch_mldata
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import pandas as pd
