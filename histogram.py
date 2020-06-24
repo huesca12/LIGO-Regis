3b18d1c92db74b79768161dbc6d5acc83ddbfaed
Nicolas Casey <nickcaseycipres@gmail.com>
2020-06-24 22:03:58 +0100

    removed redundant data preparation

--- histogram.py
+++ histogram.py
@@ -2,14 +2,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Data Prep
-#Define file location
-filename = "data/gspy_o3b.csv"
-
-rawDf = pd.read_csv(filename)
-dropList = ['chisq','chisqDof','confidence','GPStime','ifo','imgUrl','id']
-mainDf = rawDf.drop(columns=dropList)
-
 #Specify the label for analysis via user input
 glitch_type = str(input("Enter the glitch label (e.g. Scattered_Light): "))
 glitch = mainDf['label']==glitch_type
