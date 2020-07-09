26d2a2c718ac16809a1803b83f455a28f3a62eb4
huesca12 <thebritishspy12@gmail.com>
2020-07-09 20:08:54 +0100

    shallowforest overhaul

--- randomforest/viz.py
+++ randomforest/viz.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 def treeViz(X, y, clf):
     fn=list(X.columns.values)
     cn=list(y.unique())
-    fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (50,15), dpi=300)
+    fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (100,25), dpi=300)
     tree.plot_tree(clf,
                    feature_names = fn,
-                   class_names=cn,
-                   filled = True);
+                   class_names = cn,
+                   filled = True,
+                   fontsize = 10,
+                   label = "all",
+                   impurity = False);
     fig.savefig('tree.png')
